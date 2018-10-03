ca7943cdeb565448db0dfa77d13e1218ddf90500
vkk3 <43628691+vkk3@users.noreply.github.com>
2018-10-02 21:26:42 -0400

    hw5
    h2

--- server.js
+++ server.js
@@ -3,15 +3,18 @@
 const express = require('express');
 
 
+const app = express();
 
 app.get('/', (request, response) => {
     response.send('Hello MGT-656/660! Leaders for Business and Society');
 
 });
+
 app.get('/nickname', (request, response) => {
     response.send('thankful-lion');
 
 });
+
 
 
 app.listen(process.env.PORT || 4000);
