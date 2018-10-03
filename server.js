ad7c01a107f0798400bf593a8fc5ef226ee5ae33
vkk3 <43628691+vkk3@users.noreply.github.com>
2018-10-02 21:31:05 -0400

    h2
    h2

--- server.js
+++ server.js
@@ -5,15 +5,18 @@ const express = require('express');
 
 const app = express();
 
+
 app.get('/', (request, response) => {
     response.send('Hello MGT-656/660! Leaders for Business and Society');
 
 });
 
+
 app.get('/nickname', (request, response) => {
     response.send('thankful-lion');
 
 });
+
 
 
 
